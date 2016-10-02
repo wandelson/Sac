c29b93f349e8255d1bd2e3a210ec7e2720d7d6eb
Wandelson <Wandelson@WANS>
2016-10-02 18:47:46 -0300

    Add project files.

---
+++ Sac/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sac.MvcApplication" Language="C#" %>
---
+++ Sac/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sac.Startup))]
+namespace Sac
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
